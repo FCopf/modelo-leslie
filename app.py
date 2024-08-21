cc3525ef6930e1c041c4ce5c3e88a8a09999ec23
FCopf <fabiocopf@gmail.com>
2024-08-21 16:42:22 -0300

    matriz para data frame

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 from aux_fun import cria_matriz_leslie, projeta_populacao
@@ -11,61 +12,67 @@ grupos_etarios = ['Recém nascidos', 'Juvenis', 'Adultos jovens', 'Adultos velho
 # Configuração do título do aplicativo
 st.title("Modelo de Leslie para Projeção Populacional")
 
-# Configuração dos sliders na barra lateral para ajustar os parâmetros da matriz Leslie
-st.sidebar.header("Parâmetros do Modelo de Leslie")
+with st.sidebar:
+    # Configuração dos sliders na barra lateral para ajustar os parâmetros da matriz Leslie
+    st.header("Parâmetros do Modelo de Leslie")
 
-b3 = st.sidebar.slider(f"Taxa de natalidade de {grupos_etarios[2]} (b3)", min_value=0.0, max_value=5.0, value=1.2, step=0.01)
-b4 = st.sidebar.slider(f"Taxa de natalidade de {grupos_etarios[3]} (b4)", min_value=0.0, max_value=5.0, value=1.5, step=0.01)
-s1 = st.sidebar.slider(f"Taxa de sobrevivência de {grupos_etarios[0]} (s1)", min_value=0.0, max_value=1.0, value=0.6, step=0.01)
-s2 = st.sidebar.slider(f"Taxa de sobrevivência de {grupos_etarios[1]} (s2)", min_value=0.0, max_value=1.0, value=0.7, step=0.01)
-s3 = st.sidebar.slider(f"Taxa de sobrevivência de {grupos_etarios[2]} (s3)", min_value=0.0, max_value=1.0, value=0.8, step=0.01)
+    b3 = st.slider(f"Taxa de natalidade de {grupos_etarios[2]} (b3)", min_value=0.0, max_value=5.0, value=1.2, step=0.01)
+    b4 = st.slider(f"Taxa de natalidade de {grupos_etarios[3]} (b4)", min_value=0.0, max_value=5.0, value=1.5, step=0.01)
+    s1 = st.slider(f"Taxa de sobrevivência de {grupos_etarios[0]} (s1)", min_value=0.0, max_value=1.0, value=0.6, step=0.01)
+    s2 = st.slider(f"Taxa de sobrevivência de {grupos_etarios[1]} (s2)", min_value=0.0, max_value=1.0, value=0.7, step=0.01)
+    s3 = st.slider(f"Taxa de sobrevivência de {grupos_etarios[2]} (s3)", min_value=0.0, max_value=1.0, value=0.8, step=0.01)
 
-# Configuração do slider para definir o número de períodos de projeção
-t = st.sidebar.slider("Número de períodos de projeção (t)", min_value=1, max_value=100, value=100)
+    # Configuração do slider para definir o número de períodos de projeção
+    t = st.slider("Número de períodos de projeção (t)", min_value=1, max_value=100, value=100)
 
-# Configuração dos sliders para definir os estados iniciais da população
-st.sidebar.header("Estados Iniciais da População")
 
-v0_1 = st.sidebar.slider(f"Estado inicial de {grupos_etarios[0]}", min_value=0, max_value=500, value=100)
-v0_2 = st.sidebar.slider(f"Estado inicial de {grupos_etarios[1]}", min_value=0, max_value=500, value=80)
-v0_3 = st.sidebar.slider(f"Estado inicial de {grupos_etarios[2]}", min_value=0, max_value=500, value=30)
-v0_4 = st.sidebar.slider(f"Estado inicial de {grupos_etarios[3]}", min_value=0, max_value=500, value=10)
+    # Configuração dos sliders para definir os estados iniciais da população
+    st.header("Estados Iniciais da População")
 
-# Criar a matriz Leslie e o vetor de população inicial
-L = cria_matriz_leslie(b3, b4, s1, s2, s3)
+    v0_1 = st.slider(f"Estado inicial de {grupos_etarios[0]}", min_value=0, max_value=500, value=100)
+    v0_2 = st.slider(f"Estado inicial de {grupos_etarios[1]}", min_value=0, max_value=500, value=80)
+    v0_3 = st.slider(f"Estado inicial de {grupos_etarios[2]}", min_value=0, max_value=500, value=30)
+    v0_4 = st.slider(f"Estado inicial de {grupos_etarios[3]}", min_value=0, max_value=500, value=10)
+
+# Criar o vetor de população inicial e a matriz Leslie
 v0 = np.array([v0_1, v0_2, v0_3, v0_4])
+L = cria_matriz_leslie(b3, b4, s1, s2, s3)
+L_df = pd.DataFrame(L)
+L_df['Grupos Etários'] = grupos_etarios
+L_df.set_index('Grupos Etários', inplace=True)
+L_df.columns = grupos_etarios
 
-col01, col02, col03 = st.columns([0.2, 0.4, 0.4])
+# Calcular o autovalor dominante (taxa de crescimento populacional) e o autovetor correspondente
+eigenvalues, eigenvectors = np.linalg.eig(L)
+dominant_eigenvalue = np.max(np.real(eigenvalues))
 
-with col01:
-    # Exibir a matriz de Leslie
-    st.subheader("Estado inicial")
-    st.write(v0)
-    
-
-with col02:
-    # Exibir a matriz de Leslie
-    st.subheader("Matriz de Transição")
-    st.write(L)
-
-with col03:
-    # Calcular o autovalor dominante (taxa de crescimento populacional) e o autovetor correspondente
-    eigenvalues, eigenvectors = np.linalg.eig(L)
-    dominant_eigenvalue = np.max(np.real(eigenvalues))
-
-    # Exibir a taxa de crescimento populacional
-    if dominant_eigenvalue > 1:
-        st.success(f'População cresce a uma taxa de {dominant_eigenvalue:.4f} ao ano.', icon="📈")
-    elif dominant_eigenvalue < 1:
-        st.warning(f'População DECRESCE a uma taxa de {dominant_eigenvalue:.4f} ao ano', icon="📉")
-    else:
-        st.info(f'População estável. Taxa de crescimento: {dominant_eigenvalue:.4f} ao ano', icon="ℹ️")
-    
+col01, col02 = st.columns([0.35, 0.65])
 
 # Projeção da população ao longo do tempo
 populacao = projeta_populacao(L, v0, t)
 populacao = np.array(populacao)  # Convertendo para array para facilitar o plot
+populacao_df = pd.DataFrame(populacao.T)
+populacao_df['Grupos Etários'] = grupos_etarios
+populacao_df.set_index('Grupos Etários', inplace=True)
 
+with col01:
+    # Exibir a matriz de Leslie
+    st.subheader("Matriz de Transição")
+    st.write(L_df)    
+
+with col02:
+    # Exibir a matriz de Leslie
+    st.subheader("Estado no tempo t")
+    st.write(populacao_df)
+
+# Exibir a taxa de crescimento populacional
+if dominant_eigenvalue > 1:
+    st.success(f'População cresce a uma taxa de {dominant_eigenvalue:.4f} ao ano.', icon="📈")
+elif dominant_eigenvalue < 1:
+    st.warning(f'População DECRESCE a uma taxa de {dominant_eigenvalue:.4f} ao ano', icon="📉")
+else:
+    st.info(f'População estável. Taxa de crescimento: {dominant_eigenvalue:.4f} ao ano', icon="ℹ️")
+    
 # Criação das colunas para os gráficos
 col1, col2 = st.columns(2)
 
